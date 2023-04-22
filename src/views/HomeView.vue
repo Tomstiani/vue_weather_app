bf8c002faa2c0e6a79165adc2e2739dfdeb4b157
Tom Stian <toms.ingebretson@gmail.com>
2023-04-22 13:30:58 +0200

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+console.log("Hello from HomeView.vue");
+</script>
+
+<template>
+  <div>Hei</div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    '.src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'bg-dark': '#212434',
+        'bg-light': '#3F446F',
+        'yellow': '#FFD082',
+        'red': '#FF8282',
+        'silver': '#BBBBBB',
+        'alto': '#D7D7D7',
+      }
+    },
+    fontFamily: {
+      Roboto: ['Roboto', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
+
